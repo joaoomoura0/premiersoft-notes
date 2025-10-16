d568a58925079f6077fcd090f2669cde8b22d374
joaoomoura0 <mourajoaovictor325@gmail.com>
2025-10-16 16:57:42 -0300

    iniciando sistema Staff - criação modelos

---
+++ src/main/java/com/nfs/PremierNotes/diarioOcorrencias/model/DiarioOcorrenciaModel.java
@@ -0,0 +1,46 @@
+package com.nfs.PremierNotes.diarioOcorrencias.model;
+
+import com.nfs.PremierNotes.colaboradores.models.ColaboradorSeguroModel;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+    @Getter
+    @Setter
+    @Entity
+    @Table(name = "diario_ocorrencias")
+    public class DiarioOcorrenciaModel {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @NotNull(message = "A data da ocorrência é obrigatória.")
+        private LocalDate data;
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "colaborador_id", nullable = false)
+        @NotNull(message = "O colaborador é obrigatório.")
+        private ColaboradorSeguroModel colaborador;
+
+
+            @Enumerated(EnumType.STRING)
+            @NotNull(message = "O tipo de ocorrência é obrigatório.")
+            private TipoOcorrencia tipo;
+
+            @Enumerated(EnumType.STRING)
+            @NotNull
+            private StatusOcorrencia status;
+
+
+        @Size(max = 1000, message = "A descrição não pode exceder 1000 caracteres.")
+        @Column(columnDefinition = "TEXT")
+        private String descricao;
+
+        private LocalDateTime dataRegistro;
+    }
