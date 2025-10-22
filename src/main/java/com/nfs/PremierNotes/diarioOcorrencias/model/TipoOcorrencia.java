79733b4526016f235a7e16fe86dfc2dfcdaa3578
joaoomoura0 <mourajoaovictor325@gmail.com>
2025-10-22 15:49:34 -0300

    add novos tipos de ocorrencias

--- src/main/java/com/nfs/PremierNotes/diarioOcorrencias/model/TipoOcorrencia.java
+++ src/main/java/com/nfs/PremierNotes/diarioOcorrencias/model/TipoOcorrencia.java
@@ -1,7 +1,16 @@
 package com.nfs.PremierNotes.diarioOcorrencias.model;
 
     public enum TipoOcorrencia {
+        AFASTAMENTO,
         ATESTADO,
+        HORAS_EXTRAS_COMPENSADAS,
+        ERRO_APONTAMENTO,
+        ESQUECIMENTO,
         FERIAS,
-        ERRO_APONTAMENTO;
+        FOLGA,
+        NAO_ATUOU,
+        ONBOARDING,
+        SAIDA_ANTECIPADA,
+        TRABALHOU_EM_OUTRO_PROJETO,
+        TROCA_FERIADO;
     }
