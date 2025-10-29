0c820f9da1625f7d3e3dfcf752f4ac1060d56923
joaoomoura0 <mourajoaovictor325@gmail.com>
2025-10-29 16:04:51 -0300

    alteracao no nome, nomes com espaçamentos e sem _

--- src/main/java/com/nfs/PremierNotes/diarioOcorrencias/model/TipoOcorrencia.java
+++ src/main/java/com/nfs/PremierNotes/diarioOcorrencias/model/TipoOcorrencia.java
@@ -1,16 +1,26 @@
 package com.nfs.PremierNotes.diarioOcorrencias.model;
 
     public enum TipoOcorrencia {
-        AFASTAMENTO,
-        ATESTADO,
-        HORAS_EXTRAS_COMPENSADAS,
-        ERRO_APONTAMENTO,
-        ESQUECIMENTO,
-        FERIAS,
-        FOLGA,
-        NAO_ATUOU,
-        ONBOARDING,
-        SAIDA_ANTECIPADA,
-        TRABALHOU_EM_OUTRO_PROJETO,
-        TROCA_FERIADO;
+        AFASTAMENTO("Afastamento"),
+        ATESTADO("Atestado"),
+        HORAS_EXTRAS_COMPENSADAS("Horas Extras Compensadas"),
+        ERRO_APONTAMENTO("Erro de Apontamento"),
+        ESQUECIMENTO("Esquecimento de Apontamento"),
+        FERIAS("Férias"),
+        FOLGA("Folga"),
+        NAO_ATUOU("Não Atuou"),
+        ONBOARDING("Onboarding"),
+        SAIDA_ANTECIPADA("Saída Antecipada"),
+        TRABALHOU_EM_OUTRO_PROJETO("Trabalhou em Outro Projeto"),
+        TROCA_FERIADO("Troca de Feriado");
+
+        private final String descricao;
+
+        TipoOcorrencia(String descricao) {
+            this.descricao = descricao;
+        }
+
+        public String getDescricao() {
+            return descricao;
+        }
     }
