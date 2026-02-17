4e431f13bfc38592e659b565f92e02ab997b5aec
joaoomoura0 <mourajoaovictor325@gmail.com>
2026-02-17 11:47:46 -0300

    model cliente

---
+++ src/main/java/com/nfs/PremierNotes/Apontamento/Model/Cliente.java
@@ -0,0 +1,23 @@
+package com.nfs.PremierNotes.Apontamento.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "cliente")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String nomeCliente;
+}
