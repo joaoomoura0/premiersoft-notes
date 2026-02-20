ad5a58896ae53b9a43385f5608299595336858f8
joaoomoura0 <mourajoaovictor325@gmail.com>
2026-02-20 15:44:13 -0300

    adicionando list para permitir sistema buscar semana

--- src/main/java/com/nfs/PremierNotes/Apontamento/Repository/ApontamentoSemanalRepository.java
+++ src/main/java/com/nfs/PremierNotes/Apontamento/Repository/ApontamentoSemanalRepository.java
@@ -4,9 +4,11 @@ import com.nfs.PremierNotes.Apontamento.Model.ApontamentoSemanal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface ApontamentoSemanalRepository  extends JpaRepository<ApontamentoSemanal, Long> {
 
     void deleteByDataBetween(LocalDate inicio, LocalDate fim);
+    List<ApontamentoSemanal> findByDataBetween(LocalDate inicio, LocalDate fim);
 
 }
